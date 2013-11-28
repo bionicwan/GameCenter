1fd9dc3ef4f35c634811941b5edcee039839e4a3
unknown <juancho989@gmail.com>
2013-11-28 10:51:30 -0600

    Se agrega registro de user

---
+++ src/com/interalia/kelloggs/gamecenter/RegistrationActivity.java
@@ -0,0 +1,15 @@
+package com.interalia.kelloggs.gamecenter;
+
+import android.app.Activity;
+
+import com.interalia.kelloggs.gamecenter.web.ResponseInterface;
+
+public class RegistrationActivity extends Activity implements ResponseInterface {
+
+	@Override
+	public void onResultResponse(Object obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
